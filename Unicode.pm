31b9b1477e58557c74b24782ad693438f2ba483c
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2025-03-12 15:07:22 +0100

    Add check_array_unicode_block() check

--- Unicode.pm
+++ Unicode.pm
@@ -6,12 +6,29 @@ use warnings;
 
 use Error::Pure qw(err);
 use List::Util 1.33 qw(none);
+use Mo::utils 0.06 qw(check_array);
 use Readonly;
 use Unicode::UCD 'charblocks';
 
-Readonly::Array our @EXPORT_OK => qw(check_unicode_block);
+Readonly::Array our @EXPORT_OK => qw(check_array_unicode_block check_unicode_block);
 
 our $VERSION = 0.01;
+
+sub check_array_unicode_block {
+	my ($self, $key) = @_;
+
+	if (! exists $self->{$key}) {
+		return;
+	}
+
+	check_array($self, $key);
+
+	foreach my $unicode_block (@{$self->{$key}}) {
+		_check_unicode_block($unicode_block, $key);
+	}
+
+	return;
+}
 
 sub check_unicode_block {
 	my ($self, $key) = @_;
@@ -20,8 +37,18 @@ sub check_unicode_block {
 		return;
 	}
 
-	if (none { $self->{$key} eq $_ } keys %{charblocks()}) {
-		err "Parameter '".$key."' doesn't contain value Unicode block.";
+	_check_unicode_block($self->{$key}, $key);
+
+	return;
+}
+
+sub _check_unicode_block {
+	my ($value, $key) = @_;
+
+	if (none { $value eq $_ } keys %{charblocks()}) {
+		err "Parameter '".$key."' doesn't contain valid Unicode block.",
+			'Value', $value,
+		;
 	}
 
 	return;
